e5ee631fd41cab5a76d9a57cf270ec1b8b522223
Erik Schierboom <erik_schierboom@hotmail.com>
2018-11-12 07:48:45 +0100

    Setup project structure

---
+++ src/analyzer/Analysis/AnalyzeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Exercism.Analyzers.CSharp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnalyzeController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(Guid id)
+        {
+            return "value";
+        }
+    }
+}
---
+++ test/analyzer.tests/Analysis/AnalyzeControllerTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Exercism.Analyzers.CSharp.Tests
+{
+    public class AnalyzeControllerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
