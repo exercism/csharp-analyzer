9e84fc171cac9a6b16a15d4d095b2b57c7fc95e7
Erik Schierboom <erik_schierboom@hotmail.com>
2019-03-12 18:06:41 +0100

    tests: inline statuses

--- test/Exercism.Analyzers.CSharp.IntegrationTests/Helpers/TestSolutionAnalysisRun.cs
+++ test/Exercism.Analyzers.CSharp.IntegrationTests/Helpers/TestSolutionAnalysisRun.cs
@@ -5,10 +5,10 @@ namespace Exercism.Analyzers.CSharp.IntegrationTests.Helpers
         public string Status { get; }
         public string[] Comments { get; }
 
-        public bool ApproveAsOptimal => Status == TestSolutionAnalysisStatus.ApproveAsOptimal;
-        public bool ApproveWithComment => Status == TestSolutionAnalysisStatus.ApproveWithComment;
-        public bool DisapproveWithComment => Status == TestSolutionAnalysisStatus.DisapproveWithComment;
-        public bool ReferToMentor => Status == TestSolutionAnalysisStatus.ReferToMentor;
+        public bool ApproveAsOptimal => Status == "approve_as_optimal";
+        public bool ApproveWithComment => Status == "approve_with_comment";
+        public bool DisapproveWithComment => Status == "disapprove_with_comment";
+        public bool ReferToMentor => Status == "refer_to_mentor";
 
         public TestSolutionAnalysisRun(string status, string[] comments) =>
             (Status, Comments) = (status, comments);
