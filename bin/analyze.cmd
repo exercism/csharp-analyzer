20b852740cf2109a6715d9f55ece1be7f1fa6683
Erik Schierboom <erik_schierboom@hotmail.com>
2019-03-16 15:06:16 +0100

    Remove unused analyze scripts

--- bin/analyze.cmd
+++
@@ -1,4 +0,0 @@
-# Usage:
-# .\bin\analyze.cmd two_fer ~/test/
-
-dotnet run -c Release -p .\src\Exercism.Analyzers.CSharp %1 %2
--- bin/analyze.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Usage:
-# ./bin/analyze.sh two_fer ~/test/
-
-dotnet run -c Release -p ./src/Exercism.Analyzers.CSharp $1 $2
