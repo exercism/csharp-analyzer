af58e17c9c3864dca07bb67cc5d754148a5dbde8
Erik Schierboom <erik_schierboom@hotmail.com>
2019-04-01 15:21:52 +0200

    two-fer: fix invalid test

--- test/Exercism.Analyzers.CSharp.IntegrationTests/Solutions/TwoFer/Interpolation/Null/Variable/TwoFer.cs
+++ test/Exercism.Analyzers.CSharp.IntegrationTests/Solutions/TwoFer/Interpolation/Null/Variable/TwoFer.cs
@@ -4,6 +4,7 @@ public static class TwoFer
 {
     public static string Name(string input = null)
     {
-        return $"One for {(input == null ? "you" : input)}, one for me.";
+        var name = input == null ? "you" : input;
+        return $"One for {name}, one for me.";
     }
 }
