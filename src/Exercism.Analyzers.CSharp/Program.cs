582721c741c67e7fa859485802e6c9cc0858d72b
Erik Schierboom <erik_schierboom@hotmail.com>
2023-08-10 08:55:23 +0200

    Remove CommandLineParser

--- src/Exercism.Analyzers.CSharp/Program.cs
+++ src/Exercism.Analyzers.CSharp/Program.cs
@@ -1,30 +1,33 @@
 using System.Linq;
 
-using CommandLine;
-
 using Serilog;
 
 namespace Exercism.Analyzers.CSharp
 {
+    internal record Options(string Slug, string InputDirectory, string OutputDirectory);
+
     public static class Program
     {
         public static void Main(string[] args)
         {
-            Logging.Configure();
+            var options = new Options(args[0], args[1], args[2]);
 
-            Parser.Default.ParseArguments<Options>(args)
-                .WithParsed(RunAnalysis);
+            RunAnalysis(options);
         }
 
         private static void RunAnalysis(Options options)
         {
-            Log.Information("Analyzing {Exercise} solution in directory {Directory}", options.Slug, options.InputDirectory);
+            Log.Information("Analyzing {Exercise} solution in directory {Directory}", options.Slug,
+                options.InputDirectory);
 
             var solution = SolutionParser.Parse(options);
             var solutionAnalysis = SolutionAnalyzer.Analyze(solution);
             SolutionAnalysisWriter.WriteToFile(options, solutionAnalysis);
 
-            Log.Information("Analyzed {Exercise} solution in directory {Directory}. Status: {Status}. Comments: {Comments}", options.Slug, options.OutputDirectory, solutionAnalysis.Status, solutionAnalysis.Comments.Select(comment => comment.Comment));
+            Log.Information(
+                "Analyzed {Exercise} solution in directory {Directory}. Status: {Status}. Comments: {Comments}",
+                options.Slug, options.OutputDirectory, solutionAnalysis.Status,
+                solutionAnalysis.Comments.Select(comment => comment.Comment));
         }
     }
 }
