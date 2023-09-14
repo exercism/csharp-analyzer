a6564a635834aa0c18720c6d4d466c9ce35be05b
Erik Schierboom <erik_schierboom@hotmail.com>
2023-09-14 14:05:07 +0200

    Implement gigasecond analyzer

--- src/Exercism.Analyzers.CSharp/Exercises/GigasecondAnalyzer.cs
+++ src/Exercism.Analyzers.CSharp/Exercises/GigasecondAnalyzer.cs
@@ -1,3 +1,5 @@
+using System;
+
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
@@ -8,34 +10,52 @@ internal class GigasecondAnalyzer : ExerciseAnalyzer
 {
     protected override void AnalyzeExerciseSpecific(Solution solution)
     {
-        var visitor = new SyntaxWalker(solution.Compilation);
+        var visitor = new SyntaxWalker(solution.Compilation, Analysis);
 
         foreach (var syntaxTree in solution.Compilation.SyntaxTrees)
             visitor.Visit(syntaxTree.GetRoot());
-
-        if (!visitor.UsesDateTimeAddSeconds)
-            Analysis.Comments.Add(Comments.UseAddSeconds);
-        
-        base.Analyze(solution);
     }
 
     private class SyntaxWalker : CSharpSyntaxWalker
     {
         private readonly Compilation _compilation;
+        private readonly Analysis _analysis;
 
-        public bool UsesDateTimeAddSeconds;
-
-        public SyntaxWalker(Compilation compilation) => _compilation = compilation;
+        public SyntaxWalker(Compilation compilation, Analysis analysis) =>
+            (_compilation, _analysis) = (compilation, analysis);
 
         public override void VisitInvocationExpression(InvocationExpressionSyntax node)
         {
             var semanticModel = _compilation.GetSemanticModel(node.SyntaxTree);
             var invocationSymbol = semanticModel.GetSymbolInfo(node);
 
-            if (invocationSymbol.ToString() == "System.DateTime.AddSeconds(double)")
-                UsesDateTimeAddSeconds = true;
+            switch (invocationSymbol.Symbol?.ToString())
+            {
+                case "System.DateTime.AddSeconds(double)":
+                    _analysis.Comments.Add(Comments.UseAddSeconds);
+
+                    var argument = node.ArgumentList.Arguments[0].Expression.ToString();
+                    if (argument.Contains('e', StringComparison.OrdinalIgnoreCase) || !argument.Contains("_"))
+                        _analysis.Comments.Add((Comments.UseScientificNotationOrDigitSeparators(argument)));
+
+                    break;
+                case "System.Math.Pow(double, double)" when node.ArgumentList.ToString() == "(10,9)":
+                    _analysis.Comments.Add(Comments.UseScientificNotationNotMathPow(node.ToString()));
+                    break;
+            }
 
             base.VisitInvocationExpression(node);
+        }
+
+        public override void VisitObjectCreationExpression(ObjectCreationExpressionSyntax node)
+        {
+            var semanticModel = _compilation.GetSemanticModel(node.SyntaxTree);
+            var methodSymbol = semanticModel.GetSymbolInfo(node);
+
+            if (methodSymbol.ToString() == "System.DateTime.DateTime(long)")
+                _analysis.Comments.Add(Comments.DoNotCreateDateTime);
+            
+            base.VisitObjectCreationExpression(node);
         }
     }
 
