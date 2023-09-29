3a8133fb56bee38ba0b352768f3eb3a98c01dd86
Erik Schierboom <erik_schierboom@hotmail.com>
2023-09-29 14:37:09 +0200

    Fix

--- tests/reverse-string/approaches/span/Span.cs
+++ tests/reverse-string/approaches/span/Span.cs
@@ -1,6 +1,14 @@
-Span<char> chars = stackalloc[input.Length];
-for (var i = 0; i < input.Length; i++)
+using System;
+
+public static class ReverseString
 {
-    chars[input.Length - 1 - i] = input[i];
+    public static string Reverse(string input)
+    {
+        Span<char> chars = stackalloc char[input.Length];
+        for (var i = 0; i < input.Length; i++)
+        {
+            chars[input.Length - 1 - i] = input[i];
+        }
+        return new string(chars);
+    }
 }
-return new string(chars);
