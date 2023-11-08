bfc4af8acad8676234c85b3430e34d53769c48e8
Erik Schierboom <erik_schierboom@hotmail.com>
2023-11-08 09:28:35 +0100

    Add modulo operator

---
+++ tests/tags/constructs/compound-assignment/CompoundAssignment.cs
@@ -0,0 +1,16 @@
+﻿namespace DefaultNamespace;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var i = 0;
+        i += 1;
+        i -= 2;
+        i *= 3;
+        i /= 4;
+        i %= 5;
+        i <<= 6;
+        i >>= 7;
+    }
+}
